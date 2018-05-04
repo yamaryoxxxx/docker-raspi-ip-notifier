6b61f53253d9a3a2928902847157fe7b344ae0ff
yamaryoxxxx <34907379+yamaryoxxxx@users.noreply.github.com>
2018-05-04 14:15:44 +0900

    Update mail-my-ip.py

--- mail-my-ip.py
+++ mail-my-ip.py
@@ -2,22 +2,24 @@ import argparse
 import smtplib
 from email.mime.text import MIMEText
 import commands
+import time
 
 # parse args
 parser = argparse.ArgumentParser(description='Send private IP info via email.')
 parser.add_argument('--sendto', type=str, help='TO addr', required=True)
 parser.add_argument('--sendfrom', type=str, help='FROM Gmail addr', required=True)
 parser.add_argument('--password', type=str, help='Password for FROM addr', required=True)
+parser.add_argument('--wait', type=int, default=15, help='wait time before send email(sec)')
+parser.add_argument('--host', type=str, help='hostname', required=True)
 args = parser.parse_args()
 
 # get info
+time.sleep(args.wait)
 ifconfig = commands.getstatusoutput('ifconfig')[1]
-hostname = commands.getstatusoutput('hostname')[1]
 
 print ifconfig
-print hostname
 # title and body
-title = 'RasPi network info: %s' % hostname
+title = 'RasPi network info: %s' % args.host
 body = ifconfig
 
 # send mail

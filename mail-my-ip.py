544d32b85875e3f87f0d1b1b6872334a77b7a7c4
yamaryoxxxx <34907379+yamaryoxxxx@users.noreply.github.com>
2018-05-04 13:58:39 +0900

    Update mail-my-ip.py

--- mail-my-ip.py
+++ mail-my-ip.py
@@ -1,28 +1,32 @@
-import subprocess
+import argparse
 import smtplib
-import socket
 from email.mime.text import MIMEText
-import datetime
-# Change to your own account information
-to = 'me@example.com'
-gmail_user = 'test@gmail.com'
-gmail_password = 'yourpassword'
+import commands
+
+# parse args
+parser = argparse.ArgumentParser(description='Send private IP info via email.')
+parser.add_argument('--to', type=str, help='TO addr', required=True)
+parser.add_argument('--from', type=str, help='FROM Gmail addr', required=True)
+parser.add_argument('--password', type=str, help='Password for FROM addr', required=True)
+args = parser.parse_args()
+
+# get info
+ifconfig = commands.getstatusoutput('ifconfig')
+hostname = commands.getstatusoutput('hostname')
+
+# title and body
+title = 'RasPi network info: %s' % hostname
+body = ifconfig
+
+# send mail
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo
-smtpserver.login(gmail_user, gmail_password)
-today = datetime.date.today()
-# Very Linux Specific
-arg='ip route list'
-p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
-data = p.communicate()
-split_data = data[0].split()
-ipaddr = split_data[split_data.index('src')+1]
-my_ip = 'Your ip is %s' %  ipaddr
-msg = MIMEText(my_ip)
-msg['Subject'] = 'IP For RaspberryPi on %s' % today.strftime('%b %d %Y')
-msg['From'] = gmail_user
-msg['To'] = to
-smtpserver.sendmail(gmail_user, [to], msg.as_string())
+smtpserver.login(args.from, args.password)
+msg = MIMEText(body)
+msg['Subject'] = title
+msg['From'] = args.from
+msg['To'] = args.to
+smtpserver.sendmail(args.from, [args.to], msg.as_string())
 smtpserver.quit()
